54313aebfa771ffe21811b354df0ba089e9cf8de
Juan Diego Ocampo <jd.ocampom0117@icloud.com>
2020-12-26 16:33:00 -0500

    Initial Commit

---
+++ Twittermenti/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Twittermenti
+//
+//  Created by Juan Diego Ocampo on 26/12/2020.
+//  Copyright © 2020 Juan Diego Ocampo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // MARK:- Properties
+    @IBOutlet weak var backgroundView: UIView!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var sentimentLabel: UILabel!
+
+    // MARK:- Methods
+    
+    /// Tag: viewDidLoad()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    /// Tag: predictPressed()
+    @IBAction func predictPressed(_ sender: Any) {
+    
+    }
+    
+}
